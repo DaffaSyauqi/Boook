358c4569f685932a1c648fcaa80f79f058cbcd6b
DaffaSyauqi <daffasyauqi77.com>
2025-11-03 10:07:53 +0700

    build(prisma): add table star and starpercent in schema prisma

---
+++ prisma/migrations/20251103030729_add_star_and_starpercent_table/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "productStarPercent" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "times" INTEGER NOT NULL,
+    "star" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "productStarPercent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "productStar" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "receivedStars" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "productStar_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "productStarPercent" ADD CONSTRAINT "productStarPercent_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "productStar" ADD CONSTRAINT "productStar_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
