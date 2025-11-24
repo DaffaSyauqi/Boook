82780accf262d2b2e48aad43210ecb0cb421bfaa
DaffaSyauqi <daffasyauqi77@gmail.com>
2025-11-24 21:07:04 +0700

    refactor(dialog): create a description dialog

---
+++ components/e-commerce/DescriptionDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-xl font-medium mb-4">Synopsis :</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique cum
+      magni tempora esse minus dignissimos id ullam obcaecati, debitis
+      laboriosam enim officia? Ipsum eaque cumque error nulla beatae alias aut,
+      nisi placeat saepe accusamus consequatur harum cupiditate voluptatum velit
+      perspiciatis eligendi voluptatem nemo voluptates animi dolore rerum!
+      Soluta, voluptates quae.
+    </p>
+
+    <Separator class="my-4" />
+
+    <h1 class="text-xl font-medium mb-4">Detail Information :</h1>
+    <div class="flex gap-36">
+      <div class="flex-col space-y-6">
+        <div class="flex-col">
+          <h2 class="text-md">Page</h2>
+          <p class="text-sm">240</p>
+        </div>
+        <div class="flex-col">
+          <h2 class="text-md">Weight</h2>
+          <p class="text-sm">0,50 kg</p>
+        </div>
+      </div>
+      <div class="flex-col space-y-6">
+        <div class="flex-col">
+          <h2 class="text-md">Long</h2>
+          <p class="text-sm">20 cm</p>
+        </div>
+        <div class="flex-col">
+          <h2 class="text-md">Wide</h2>
+          <p class="text-sm">18 cm</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
