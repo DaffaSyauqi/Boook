7b6a4e617b9d763a999731f6cbb55b0e47c55b31
DaffaSyauqi <daffasyauqi77@gmail.com>
2026-02-18 19:49:41 +0700

    feat(folder): add and move image to illustration

--- app/components/landing/Hero.vue
+++ app/components/landing/Hero.vue
@@ -5,7 +5,7 @@ const data = {
   title: "Navigating the digital landscape for success",
   description:
     "Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.",
-  image: "hero.png",
+  image: "illustration/hero.png",
 };
 </script>
 
